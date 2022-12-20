55ea306128a4c0879fe70d35a5b80672f4f0d7da
ivansansao <aivanplus@gmail.com>
2022-12-19 23:18:11 -0300

    Commit

--- src/models/Car.js
+++ src/models/Car.js
@@ -783,9 +783,14 @@ class Car {
                     stroke(255, 0, 0);
                     strokeWeight(1);
                     lineX(ray.pos.x, ray.pos.y, menorHit.x, menorHit.y, 'hsl(0, 100%, 70%)');
+                    stroke(this.cor);
                     circle(menorHit.x, menorHit.y, 10);
-                    // text(i, menorHit.x, menorHit.y);
-                    text(ray.savedDistance, menorHit.x, menorHit.y);
+                    // fill(0, 0, 255);
+                    // strokeWeight(1);
+                    // stroke(255);
+                    // textSize(12)
+                    // // text(i, menorHit.x, menorHit.y);
+                    // text(ray.savedDistance, menorHit.x, menorHit.y);
 
                     if (this.showSensorValue) {
                         noStroke();
