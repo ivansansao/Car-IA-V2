ad1984409698d6538e914f7c5ea338897eda0913
ivansansao <aivanplus@gmail.com>
2023-08-31 19:53:43 -0300

    Server api pointed to cloud now if running not in localhost

--- src/models/Api.js
+++ src/models/Api.js
@@ -1,6 +1,7 @@
 class Api {
     constructor() {
-        this.url = 'http://localhost:1905';
+        const isCloud = window.location.origin.includes('https')
+        this.url = isCloud ? 'http://187.23.108.203:1905' : 'http://localhost:1905'
     }
 
     fetch(page, content) {
